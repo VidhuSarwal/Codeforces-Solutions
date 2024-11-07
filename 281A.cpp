03d35295c68b73bac6f2b9e59f5fbd9e3888c56a
VidhuSarwal <vidhusarwal@hotmail.com>
2024-11-07 11:58:45 +0530

    added 281A

---
+++ 281A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+
+/*
+1) Implement linear search from both sides
+2) Count number of compares it take till it finds the number
+3) output result based on that. 
+
+*/
+int main() {
+
+    
+    return 0; 
+}
